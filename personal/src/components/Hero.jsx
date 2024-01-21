60741feac777b76249a2007a460acc0ad313e8d6
ste1v0 <tyotarjous@yandex.ru>
2024-01-21 13:57:03 +0300

    feat: add hero content

--- personal/src/components/Hero.jsx
+++ personal/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { SlAnimation, SlCarousel, SlCarouselItem } from '@shoelace-style/shoelace/dist/react';
+import { Icon } from '@iconify/react';
 
 export default function Hero({lightMode, fontIncreased}) {
 
@@ -29,11 +30,27 @@ const animation = {
                         <span className="hero__tap hover">{animate ? '' : '← Tap!'}</span>
                     </SlAnimation>
                     </div>
-                    <div style={{backgroundColor: !lightMode ? 'whitesmoke' : 'purple', color: lightMode ? 'white' : 'black'}}className="hero__count capitalize">Taps count: {count}</div>
+                    {/* <div style={{backgroundColor: !lightMode ? 'whitesmoke' : 'purple', color: lightMode ? 'white' : 'black'}}className="hero__count capitalize">Taps count: {count}</div> */}
+                    <div className="hero__text">
+                        <div className="hero__name" style={{fontSize: fontIncreased ? 'var(--name-increased)': 'var(--name-default)'}}>Stepan Serov</div>
+                        <span className="hero__position" style={{fontSize: fontIncreased ? 'var(--position-increased)': 'var(--position-default)'}}>Frontend Developer</span>
+                    </div>
+                    <div className="hero__badges">
+                        <Icon className="hover rotate" icon="mdi:react" width="32" height="32" />
+                        <Icon className="hover" icon="mdi:language-javascript" width="32" height="32" />
+                        <Icon className="hover" icon="mdi:tailwind" width="32" height="32" />
+                        <Icon className="hover" icon="mdi:language-html5" width="32" height="32" />
+                        <Icon className="hover" icon="mdi:language-css3" width="32" height="32" />
+                    </div>
+                    <div className="hero__icons">
+                        <Icon icon="iwwa:file-pdf" className="hover" width="32" height="32" />  
+                        <span>CV</span>
+                    </div>
+                    
 
                 </section>
                 <section className="hero__right">
-                    <h2 className="nabla" style={{fontSize: fontIncreased ? 'var(--title-increased)': 'var(--title-default)'}}>Latest projects</h2>
+                    <h2 className="kaushan-script-regular" style={{fontSize: fontIncreased ? 'var(--title-increased)': 'var(--title-default)'}}>Latest projects</h2>
                         <SlCarousel autoplay loop pagination mouseDragging className="hero__carousel">
                             <SlCarouselItem>
                                 <a href="https://fabulous-marigold-8f871e.netlify.app"><div style={{color: lightMode ? 'black' : 'white'}} className="hero__carousel-title">Games Database</div></a>
