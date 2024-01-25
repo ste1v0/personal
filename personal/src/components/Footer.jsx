3cadcbf823b3853802c2d99137700e5033ae1c6f
ste1v0 <tyotarjous@yandex.ru>
2024-01-26 00:35:25 +0300

    init: upd files

---
+++ personal/src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import { Icon } from '@iconify/react';
+import { SlRating, SlAnimation } from '@shoelace-style/shoelace/dist/react';
+
+import Placeholder from 'react-bootstrap/Placeholder';
+
+export default function Footer({lightMode}) {
+    const [fact, setFact] = useState([])
+    const [reload, setReload] = useState(false)
+    const [rating, setRating] = useState(0)
+    const [hover, setHover] = useState('')
+
+    function getSymbol(value) {
+        const icons = ['emoji-angry', 'emoji-frown', 'emoji-expressionless', 'emoji-smile', 'emoji-laughing'];
+        return `<sl-icon name="${icons[value - 1]}"></sl-icon>`;
+    }
+
+	useEffect(function() {
+        setFact([])
+		fetch('https://dog-api.kinduff.com/api/facts')
+			.then(res => {
+				if (!res.ok) {
+					throw new Error('Something is wrong with the API')
+				}
+				return res.json()
+			})
+			.then(data => {
+				setFact(data.facts[0])
+			})
+	}, [reload])
+
+    return (
+        <footer className="fade-in-footer">
+            <div className="footer__title-container">
+                <Icon icon="fluent-emoji:dog-face" className="scale" width="32" height="32" />
+                <div className="footer__title capitalize">Random dog fact</div>
+                <span onClick={() => setReload(prevValue => !prevValue)} className="material-symbols-outlined pointer hover rotate">refresh</span>
+            </div>
+            {fact.length > 0 
+            ? <div style={{border: lightMode ? '1px dashed black' : '1px dashed white'}} className="footer__fact"> {fact}</div> 
+            : <><Placeholder animation="glow">
+                <Placeholder style={{width: '30vw'}} />{' '}
+            </Placeholder>
+            <Placeholder animation="glow">
+                <Placeholder style={{width: '30vw', marginBottom: '2vh'}} />{' '}
+            </Placeholder>
+            </>}
+            <div className="footer__rating">
+                <span>Rate this page</span>
+                {rating ? <SlAnimation name="jackInTheBox" duration={1000} iterations={1} play ><div>Thanks!</div></SlAnimation> : <SlRating className={lightMode ? `light rating--${hover}` : `dark rating--${hover}`} label="Rating" getSymbol={getSymbol} onSlHover={(event) => setHover(event.target.value)} onSlChange={() => setRating(true)} />}
+            </div>
+            <a href="https://github.com/ste1v0" target="_blank" rel="noreferrer">
+                <div className={lightMode ? "footer__icons footer__github--dark pointer" : "footer__icons footer__github--light pointer"}></div>
+            </a>
+        </footer>
+    )
+}
---
+++ personal/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.12.0/cdn/');
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<>
+		<App />
+	</>
+)
