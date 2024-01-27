7f852b6a717c16af7e842ecbbaad43d57ce465cf
ste1v0 <tyotarjous@yandex.ru>
2024-01-28 00:28:26 +0300

    feat: add home icon

--- personal/src/components/Header.jsx
+++ personal/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Icon } from '@iconify/react';
+
 export default function Header({lightMode, setLightMode, fontIncreased, setFontIncreased}) {
     
     function changeMode() {
@@ -11,7 +13,7 @@ export default function Header({lightMode, setLightMode, fontIncreased, setFontI
         <header className="fade-in-header">
             <nav style={{backgroundColor: lightMode ? '#282836' : '#CACAD3', color: lightMode ? 'white' : 'black'}}>
                 <ul className="header__container">
-                    <li style={{backgroundColor: lightMode ? '#141515' : '#EEF0F3'}} className="header__link">ste1v0.dev</li>
+                    <li style={{backgroundColor: lightMode ? '#141515' : '#EEF0F3'}} className="header__link"><Icon icon="line-md:home-md-twotone-alt" width="24" height="24" /></li>
                     <li className="material-symbols-outlined hover pointer" onClick={changeFont}>
                         <span>
                             {fontIncreased ? "text_decrease" : "text_increase"}
